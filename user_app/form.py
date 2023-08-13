537b5c1613e8dd2cc62f038a90f21266167351c1
shiccorama <shiccorama@gmail.com>
2023-08-13 16:30:02 +0300

    adjust styling of crispy form and about page

--- user_app/form.py
+++ user_app/form.py
@@ -3,7 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField() # creating new field inside the form.
+    # creating new field inside the form.
+    email = forms.EmailField() 
+    # this is how we edit the default form attributes:
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields["username"].widget.attrs.update({
+    #           "type":"text",
+    #           "id":"FirstName",
+    #           "name":"first_name",
+    #           "class":"mt-1 w-[400px] rounded-md border-green-500 bg-yellow-300 text-sm text-gray-700 shadow-sm",
+    #           "placeholder":"username",
+    #     })
 
     class Meta:
         model = User  # validate which model to compare data with .
