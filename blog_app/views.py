518b9d9003adc6382f800f5235cdc3016521f727
shiccorama <shiccorama@gmail.com>
2023-08-20 11:21:01 +0300

    using ListView, DetailView and CreateView classes

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from blog_app.models import Post
+# import ListView from generic:
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+)
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # if we're going to use HttpResponse, we will do like this :
 # from django.http import HttpResponse
@@ -23,6 +31,8 @@ from blog_app.models import Post
 #     },
 # ]
 
+
+
 def home(request):
     context = {
         "posts": Post.objects.all()
@@ -31,3 +41,31 @@ def home(request):
 
 def about(request):
     return render(request, "blog/about.html", {"title": "About Page"})
+
+
+## the pattern that url looks for to display the page correctly is :
+# <app>/<model>_<viewtype>.html ==> blog_app/Post_List.html
+
+class PostListView(ListView):
+    ''' We have taken a new class that inherits directly from ListView, and inside
+    it we have to define the model which we want to do (crud ops) on it '''
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post-detail.html"
+    context_object_name = "posts"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+    context_object_name = "posts"
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
